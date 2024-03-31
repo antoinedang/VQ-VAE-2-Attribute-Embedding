21b8f49538470a9536caaa4f4738b8c48c9d9db9
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-03-31 00:00:13 -0400

    update for smaller dataset and pre-loaded dataset class

--- augment_data.py
+++ augment_data.py
@@ -5,8 +5,8 @@ import os
 GENRES = ["blues", "classical", "country", "disco", "hiphop", "jazz", "metal", "pop", "reggae", "rock"]
 GTZAN_SAMPLE_RATE = 22050
 
-gain_transforms = [torchaudio.transforms.Vol(g) for g in [1, 0.5, 0,75, 1.25, 1.5]]
-pitch_transforms = [torchaudio.transforms.PitchShift(GTZAN_SAMPLE_RATE, n) for n in [0, -4, -2, 2, 4]]
+gain_transforms = [torchaudio.transforms.Vol(g) for g in [1, 0.5, 1.5]]
+pitch_transforms = [torchaudio.transforms.PitchShift(GTZAN_SAMPLE_RATE, n) for n in [0, -2, 2]]
 
 total_augmented_samples = 0
 iterations_completed = 0
@@ -14,7 +14,7 @@ total_iterations_required = 1000 * len(gain_transforms) * len(pitch_transforms) 
 
 if not os.path.exists("AugmentedGTZAN"): os.makedirs("AugmentedGTZAN")
 
-print("Augmenting...")
+print("Augmenting to {} samples...".format(total_iterations_required))
 
 for genre in GENRES:
   output_folder = "AugmentedGTZAN/{}".format(genre)
@@ -22,12 +22,21 @@ for genre in GENRES:
   i = 0
   raw_data_filenames = os.listdir("GTZAN/{}".format(genre))
   for raw_data_filename in raw_data_filenames:
-    raw_sample, _ = torchaudio.load("GTZAN/{}".format(genre) + "/" + raw_data_filename)
+    try:
+      raw_sample, _ = torchaudio.load("GTZAN/{}".format(genre) + "/" + raw_data_filename)
+    except:
+      continue
     for gain_transform in gain_transforms:
       for pitch_shift in pitch_transforms:
+        output_filename = output_folder + "/{}_{}.wav".format(genre, i)
+        if os.path.exists(output_filename):
+          i += 1
+          total_augmented_samples += 1
+          iterations_completed += 1
+          continue
+          
         gain_augmented_sample = gain_transform(raw_sample)
         full_augmented_sample = pitch_shift(gain_augmented_sample)
-        output_filename = output_folder + "/{}_{}.wav".format(genre, i)
         torchaudio.save(output_filename, full_augmented_sample.detach(), GTZAN_SAMPLE_RATE)
         total_augmented_samples += 1
         i += 1
--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ from torch.utils.data import Dataset
 import torchaudio
 import matplotlib.pyplot as plt
 import os
+import random
 
 GENRES = ["blues", "classical", "country", "disco", "hiphop", "jazz", "metal", "pop", "reggae", "rock"]
 GTZAN_SAMPLE_RATE = 22050
@@ -37,6 +38,7 @@ class LazyGTZANDataset(Dataset):
         for genre in GENRES:
             genre_filepaths = ["{}/{}/{}".format(dataset_folder, genre, fname) for fname in os.listdir("{}/{}".format(dataset_folder, genre))]
             self.file_paths.extend(genre_filepaths)
+        random.shuffle(self.file_paths)
 
     def __len__(self):
         return len(self.file_paths)
@@ -49,4 +51,22 @@ class LazyGTZANDataset(Dataset):
             if GENRES[i] in file_path:
                 label = i
         
-        return sample, label
+        return sample, torch.tensor(label).to(DEVICE)
+
+
+class GTZANDataset(Dataset):
+    def __init__(self, dataset_folder="GTZAN"):
+        self.samples = []
+        self.labels = []
+        for genre in GENRES:
+            genre_filepaths = ["{}/{}/{}".format(dataset_folder, genre, fname) for fname in os.listdir("{}/{}".format(dataset_folder, genre))]
+            for file_path in genre_filepaths:
+                self.samples.append(load_spectrogram_from_wav(file_path))
+                label = torch.tensor(GENRES.index(genre)).to(DEVICE)
+                self.labels.append(label)
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        return self.samples[idx], self.labels[idx]
