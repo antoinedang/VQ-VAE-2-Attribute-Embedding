05a1d1fe8f0ff4a1e66c21af32bd30f7c92f2f2d
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-04-04 18:26:46 -0400

    update MSE graphing to support multiple models

--- compute_mse.py
+++ compute_mse.py
@@ -3,36 +3,37 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-folder_path = "eval_sample_1"
+folder_paths = [("eval_samples_0.1", "Margin Loss Weight 0.1"), ("eval_samples_10", "Margin Loss Weight 10")]
 
 max_images = 200
 
-mse_list =[]
-epochs = []
+for folder_path, label in folder_paths:
+    mse_list =[]
+    epochs = []
 
-for x in range(1, max_images + 1):
+    for x in range(1, max_images + 1):
 
-    filename = f'{x:05d}_00000.tiff'
+        filename = f'{x:05d}_00000.tiff'
 
-    image_path = os.path.join(folder_path, filename)
+        image_path = os.path.join(folder_path, filename)
 
-    spec = load_spectrogram_img(image_path).detach().cpu()
-    width = spec.shape[1]
-    original_spec = np.array(spec[:, :width//2])
-    reconstructed_spec = np.array(spec[:, width//2:])
+        spec = load_spectrogram_img(image_path).detach().cpu()
+        width = spec.shape[1]
+        original_spec = np.array(spec[:, :width//2])
+        reconstructed_spec = np.array(spec[:, width//2:])
 
-    mse = np.mean((np.log(original_spec+1) - np.log(reconstructed_spec+1)) ** 2)
+        mse = np.mean((np.log(original_spec+1) - np.log(reconstructed_spec+1)) ** 2)
 
-    mse_list.append(mse)
-    epochs.append(x)
+        mse_list.append(mse)
+        epochs.append(x)
 
-# plt.plot(epochs)
-plt.plot(mse_list)
+    # plt.plot(epochs)
+    plt.plot(epochs, mse_list, label=label)
 
-plt.xlabel('Epoch Number')
-plt.ylabel('Normalized MSE Value')
-plt.title('Normalized MSE Value in terms of epoch number')
+plt.xlabel('# Epochs')
+plt.ylabel('MSE')
 
+plt.legend()
 plt.show()
 
 
